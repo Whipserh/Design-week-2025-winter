ff3778e968eca4c9c0073caf17ffbfd2294e9710
JudeLadouceur <jude.m.ladouceur@gmail.com>
2025-03-06 13:05:21 -0500

    Created lever movement

--- Assets/Script/Puzzles/BreakerBox/LeverBehavior.cs
+++ Assets/Script/Puzzles/BreakerBox/LeverBehavior.cs
@@ -2,18 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D)), RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class LeverBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera mainCam;
+
+    private const float maximumHeightDist = 0.625f;
+
+    private Vector2 leverInitialPos;
+    private Vector2 mouseInitialPos;
+
+    private void Start()
     {
-        
+        mainCam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        leverInitialPos = transform.localPosition;
+        mouseInitialPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        //gets how far the mouse is from where you clicked it
+        Vector2 distanceTravelled = mouseInitialPos - mousePos;
+
+        //adds that distance travelled from the last frame to the game object
+        transform.localPosition = new Vector2(0, Mathf.Clamp(leverInitialPos.y - distanceTravelled.y, -maximumHeightDist, maximumHeightDist));
     }
 }
